8acb61ecdcad83f2bf927c448c33a46386123404
Subhrajyoti Adhikary <105319415+Subhrajyoti100@users.noreply.github.com>
2022-05-15 23:05:30 +0530

    Update CircularOrNot.java

--- CircularOrNot.java
+++ CircularOrNot.java
@@ -1,1 +1,15 @@
-
+public class {
+public static boolean TrueOrFalse(Node head){
+  Node fast = head;
+  Node slow = head;
+  while(fast!=null && fast.next==null){
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  if(slow==fast){
+    return true;
+  }
+  return false;
+}
+}
+  
